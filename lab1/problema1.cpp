5d4b657c5a11fa0b2671f6a0f25aba90554e4696
dinfdo <dfdavidqd7@gmail.com>
2025-03-02 15:36:39 +0200

    lab 1

--- lab1/problema1.cpp
+++ lab1/problema1.cpp
@@ -1,21 +1,51 @@
-#include <iostream>
+#include <stdio.h>
+#include <cstring>
 using namespace std;
 
-bool isPrime(int n)
-{
-    for (int tr = 2; tr < n / 2; tr++)
-        if ((n % tr) == 0)
-            return false;
-    return true;
+float convert2float(char v[]) {
+    float val = 0.0f;
+    float decimfac = 1.0f;
+    bool Negativ = false;
+    bool hasdecim = false;
+    int i = 0;
+
+    if (v[i] == '-') {
+        Negativ = true;
+        i++;
+    }
+
+    for (i = 0; i < strlen(v); i++) {
+        if (v[i] == '.') {
+            if (hasdecim) {
+                return 0.0f;
+            }
+            hasdecim = true;
+        } else if (v[i] >= '0' && v[i] <= '9') {
+            if (hasdecim) {
+                decimfac /= 10.0f;
+                val += (v[i] - '0') * decimfac;
+            } else {
+                val = val * 10.0f + (v[i] - '0');
+            }
+        }
+    }
+    if (Negativ) {
+        val = -val;
+    }
+    return val;
 }
-int main()
-{
-    int n;
-    std::cout << "Enter a number:";
-    std::cin >> n;
-    if (isPrime(n))
-        std::cout << n << " is prime !";
-    else
-        std::cout << n << " is NOT prime !";
+
+int main() {
+    char buffer[101];
+    float totalSum = 0.0f;
+    FILE* file;
+
+    file = fopen("in.txt", "r");
+    while (fgets(buffer, 101, file) != NULL) {
+        totalSum += convert2float(buffer);
+        strcpy(buffer, "");
+    }
+    fclose(file);
+    printf("Sum is: %0.0f", totalSum);
     return 0;
 }
--- lab1/problema2.cpp
+++ lab1/problema2.cpp
@@ -1,5 +1,5 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include <cstring>
 using namespace std;
 
 void Words(char str[]){
@@ -12,33 +12,36 @@ void Words(char str[]){
         p=strtok(NULL, " ");
 
     }
-
+	char aux[101];
     for(int k=1; k<i; k++)
         for(int j=k+1; j<=i; j++)
-            if(strlen(v[k])<strlen(v[j]))
-                swap(v[j],v[k]);
-                
+            if(strlen(v[k])<strlen(v[j])){
+                strcpy(aux,v[j]);
+				strcpy(v[j],v[k]);
+				strcpy(v[k],aux);
+			}
+			
     for(int k=1;k<i;k++)
         for(int j=k+1;j<=i;j++)
-            if(strlen(v[k])==strlen(v[j])&&strcmp(v[k], v[j])>0)
-                swap(v[j],v[k]);
-
+            if(strlen(v[k])==strlen(v[j])&&strcmp(v[k], v[j])>0){
+                strcpy(aux,v[j]);
+				strcpy(v[j],v[k]);
+				strcpy(v[k],aux);
+			}
+                              
     for(int r=1; r<=i; r++)
         printf("%s\n",v[r]);
 
 }
 
-int main()
-{
+int main(){
     char str[101];
     FILE * openFile;
 
     openFile = fopen("in.txt", "r");
 
     if(fgets (str, 101, openFile) != NULL)
-    {
-        Words(str);
-    }
+        Words(str);   
 
     fclose(openFile);
     return 0;
